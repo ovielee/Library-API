4863d75b80ae9363ea0c4105381ed731317bd7ee
ovielee <anthony.oke@outlook.com>
2025-01-28 23:49:40 +0000

    Initial commit

---
+++ models/bookModel.js
@@ -0,0 +1,14 @@
+let books = [
+    {
+      id: 'B001',
+      title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      genre: 'Classic Fiction',
+      publication_date: '1925-04-10',
+      availability: 'available',
+      edition: '1st',
+      summary: 'A novel about the corruption of the American Dream.',
+    },
+  ];
+  
+  module.exports = books;
